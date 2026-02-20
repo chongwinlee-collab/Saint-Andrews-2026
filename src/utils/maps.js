aa3ffbaa2936cb82d28e7e8a721a31a582ed4f54
chongwinlee-collab <chongwin.lee@gmail.com>
2026-02-21 00:10:10 +0800

    Delete src directory

--- src/utils/maps.js
+++
@@ -1,7 +0,0 @@
-export function openMap(lat, lon, name) {
-  const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent)
-  const url = isIOS
-    ? `https://maps.apple.com/?q=${name}&ll=${lat},${lon}`
-    : `https://www.google.com/maps?q=${lat},${lon}`
-  window.open(url, '_blank')
-}
